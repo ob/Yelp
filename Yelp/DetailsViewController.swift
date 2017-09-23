eda08244c74242f5acc38b7849b660a0fdbd985d
Oscar Bonilla <obonilla@linkedin.com>
2017-09-23 12:37:25 -0700

    Embetter the detail view.

--- Yelp/DetailsViewController.swift
+++ Yelp/DetailsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import MapKit
 
 class DetailsViewController: UIViewController {
 
@@ -17,8 +18,11 @@ class DetailsViewController: UIViewController {
     @IBOutlet weak var categoriesLabel: UILabel!
     @IBOutlet weak var openNowLabel: UILabel!
     @IBOutlet weak var openingHoursLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
 
     var business: Business?
+    let regionRadius: CLLocationDistance = 1000
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,13 +43,26 @@ class DetailsViewController: UIViewController {
         if let url = business?.ratingImageURL {
             ratingImageView.setImageWith(url)
         }
+        if let latitude = business?.latitude,
+            let longitude = business?.longitude {
+
+            let initialLocation = CLLocation(latitude: latitude, longitude: longitude)
+            centerMapOnLocation(location: initialLocation)
+            mapView.addAnnotation(business!)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius, regionRadius)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+
 
     /*
     // MARK: - Navigation
