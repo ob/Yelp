055c30c0ff2a757f825a9c6d589d9a8683835f60
Oscar Bonilla <obonilla@linkedin.com>
2017-09-23 13:28:30 -0700

    Fix a few bugs.

--- Yelp/DetailsViewController.swift
+++ Yelp/DetailsViewController.swift
@@ -32,7 +32,6 @@ class DetailsViewController: UIViewController {
         navigationController?.navigationBar.tintColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         let navigationTitleFont = UIFont(name: "Avenir", size: 20)!
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedStringKey.font: navigationTitleFont]
-        navigationController?.navigationBar.topItem?.title = "Filter"
 
         nameLabel.text = business?.name
         reviewsLabel.text = business?.prettyReviews()
